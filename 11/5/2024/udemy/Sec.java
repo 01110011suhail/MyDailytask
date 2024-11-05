dccc51dacff31be4768ca01e8eacc93c0680086b
01110011suhail <sha433415@gmail.com>
2024-11-05 17:10:16 +0530

    FirstHour

---
+++ 11/5/2024/udemy/Sec.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Random;
+
+class Sec {
+    public static void main(String[] args) {
+        int[] firstarr = getRandomArr(10);
+        System.out.println(Arrays.toString(firstarr));
+
+        Arrays.sort(firstarr);
+        System.out.println(Arrays.toString(firstarr));
+        int[] secondarr = new int[10];
+        System.out.println(Arrays.toString(secondarr));
+        Arrays.fill(secondarr, 5);
+        System.out.println(Arrays.toString(secondarr));
+
+    }
+
+    private static int[] getRandomArr(int len) {
+        Random ran = new Random();
+        int[] arr = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            arr[i] = ran.nextInt(100);
+        }
+        return arr; //
+    }
+}
---
+++ 11/5/2024/udemy/Third.java
@@ -0,0 +1,8 @@
+package 11.5.2024.udemy;
+
+public class Third {
+    public static void main(String[] args) {
+        
+    }
+    
+}
